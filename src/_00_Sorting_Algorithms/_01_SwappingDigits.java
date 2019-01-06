c9b028a1bd8d1a946f99628e1947c07cd067a5dc
League <league@WTS8.attlocal.net>
2019-01-06 11:28:56 -0800

    bongos
    im popping bongos blade

--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -8,6 +8,7 @@ class _01_SwappingDigits {
 
 	@Test
 	void test() {
+		
 		int[] x = {5, 6};
 		int[] y = {6, 5};
 		swapArrayOfTwo(x);
@@ -20,17 +21,33 @@ class _01_SwappingDigits {
 		
 		int[] m = {7, 3, 9, 5, 2};
 		assertEquals(5, findMiddle(m));
+		
 	}
 	
 	//1. Complete the method so that the first two elements of the array are swapped
 	public static void swapArrayOfTwo(int[] arr) {
-		
+		int arr1=arr[1];
+		arr[1]=arr[0];
+		arr[0]=arr1;
 	}
 	
 	//2. Complete the method so that it sorts the array using a bubble sort.
 	//   Iterate through the array and when you find two elements that are out
 	//   of order, swap them. Repeat this until the array is in order.
 	public static void sortIntArray(int[] arr) {
+		boolean swap=true;
+		while(swap) {
+			swap=false;{
+				for(int i=0;i<arr.length-1;i++) {
+					if(arr[i]>arr[i+1]) {
+						int arr1=arr[i+1];
+						arr[i+1]=arr[i];
+						arr[i]=arr1;
+						swap=true;
+					}
+				}
+			}
+		}
 		
 	}
 	
@@ -38,6 +55,8 @@ class _01_SwappingDigits {
 	//   *Hint* it helps to sort it first.
 	//   *Double Hint* Use the method you already wrote in step 2 to sort it
 	public static int findMiddle(int[] arr) {
-		return 0;
+		sortIntArray(arr);
+				return arr[(arr.length)/2];
+
 	}
 }
