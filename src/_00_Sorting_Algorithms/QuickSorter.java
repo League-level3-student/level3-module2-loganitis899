e0bd35d176f25c48bc2462e597fade298749f71c
League <league@WTS8.attlocal.net>
2019-02-24 11:27:32 -0800

    quicc
    sanik gottta go quicc

--- src/_00_Sorting_Algorithms/QuickSorter.java
+++ src/_00_Sorting_Algorithms/QuickSorter.java
@@ -21,15 +21,24 @@ public class QuickSorter extends Sorter {
 	private void quickSort(int[] array, int low, int high, SortingVisualizer display) {
 		//2. create two integer variables called i and j and set them equal
 		//   to low and high respectively.
+		int i=low;
+		int j=high;
        
         //3. create an integer called pivot and set it equal to the element
         //   in the array that is the halfway point between low and high
-
+			int pivot=(low+high)/2;
         //4. make a while loop that goes while i is less than or equal to j
+        while(i<=j) {
+       
+        
         
         	//5. make a while loop that increments i while 
         	//   the array element at i is less than pivot
-            
+             	while(array[i]<pivot) {
+        		i+=1;
+        	}
+        }
+        
             //6. make a while loop that decrements j while 
         	//   the array element at j is greater than pivot
             
--- src/_00_Sorting_Algorithms/SelectionSorter.java
+++ src/_00_Sorting_Algorithms/SelectionSorter.java
@@ -13,14 +13,14 @@ public class SelectionSorter extends Sorter {
 		//   element of the array
 		for(int i=0;i<array.length-1;i++) {
 			int index=i;
-			for(int j=i+1;j<array.length;j++){
+			for(int j=(i+1);j<array.length;j++){
 				if(array[j]<array[index]) {
-					index=j;
+					index=j;}
 					int twote=array[i];
 					array[i]=array[index];
 					array[index]=twote;
 					display.updateDisplay();
-				}
+				
 			}
 		}
 			//2. create an integer called index and set it equal to i
