5e21177197a89bbae2d04b52b9ed9707cd730830
League <league@WTS8.attlocal.net>
2019-03-10 11:26:19 -0700

    multipy
    done with sorting

--- src/_01_Searching_Algorithms/_01_BinarySearch.java
+++ src/_01_Searching_Algorithms/_01_BinarySearch.java
@@ -20,6 +20,8 @@ public class _01_BinarySearch {
 			//3. create an integer called mid and set it equal
 			//   to the half way point between start and end
 			int mid=(start+end)/2;
+			System.out.println(mid);
+			System.out.println(array.length);
             //4. if the array element at mid is equal to value
             //   then return mid
 			if(array[mid]==value) {
--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -17,26 +17,34 @@ class _04_SearchTest {
 	}
 
 	@Test
-	*/
 	public void testBinarySearch() {
-		int[] array = {1,2,3,4,5,5,5,7,9,10,15,100};
+		int[] array = {1,2,3,4,5,5,5,7,9,10,12,13};
 		int start = 1;
-		int end= 100;
-		int value=15;
+		int end= 13;
+		int value=12;
 		
 		assertEquals(10,_01_BinarySearch.binarySearch(array, start, end, value));
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
 	}
 	
-	@Test
+	@Test	
+
 	public void testInterpolationSearch() {
+		int[] array= {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30};
+		int start =2;
+		int end=30;
+		int value = 14;
+		assertEquals(6, _02_InterpolationSearch.interpolationSearch(array, value));
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
 	}
 	
-	@Test
+	@Test*/
 	public void testExponentialSearch() {
+		int[] array= {1,2,3,6,7,7,7,7,7,7,7,9,50,70,80,90,100,100,110,111,200,203,204,205};
+		int value = 111;
+		assertEquals(19, _03_ExponentialSearch.exponentialSearch(array, value));
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
 	}
