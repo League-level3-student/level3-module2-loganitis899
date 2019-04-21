0d91c80b702eced2fb9de123a1631d51cd0ae128
loganitis899 <league@WTS20.attlocal.net>
2019-04-21 11:28:13 -0700

    done
    get em outa here

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -1,10 +1,13 @@
 package _02_More_Algorithms;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import _00_Sorting_Algorithms.SortingVisualizer;
 
 public class Algorithms {
+	
 //	public static int findBrokenEgg(List<String> eggs) {
 //		for(int i=0;i<eggs.size();i++) {
 //		//	if(eggs.get(i)=="cracked") {
@@ -14,6 +17,8 @@ public class Algorithms {
 //		}
 //		return 0;
 //	}
+	
+	
 	
 //	public static int countPearls(List<Boolean> oysters) {
 //		int counter=0;
@@ -25,6 +30,8 @@ public class Algorithms {
 //		return counter;
 //	}
 	
+	
+	
 //	public static double findTallest(List<Double> peeps) {
 //		double highest=0;
 //		for(int i=1;i<peeps.size();i++) {
@@ -35,6 +42,8 @@ public class Algorithms {
 //		return highest;
 //	}
 	
+	
+	
 //	public static String findLongestWord(List<String> words) {
 //		String word=words.get(0); 
 //		for(int i=1;i<words.size();i++) {
@@ -44,6 +53,8 @@ public class Algorithms {
 //		}
 //		return word;
 //	}
+	
+	
 	
 //	public static Boolean containsSOS(List<String> message) {
 //		
@@ -56,11 +67,62 @@ public class Algorithms {
 //		return false;
 //	}
 	
-	public static Double sortScores(List<Double> grades) {
-
-		return 0.0;
-	}
 	
+	
+//	public static List<Double> sortScores(List<Double> grades) {
+//		for(int o=0;o<grades.size();o++) {
+//			for(int i=1;i<grades.size();i++) {
+//				if(grades.get(i)<grades.get(i-1)) {
+//				double replace = grades.get(i);
+//				grades.set(i, grades.get(i-1));
+//				grades.set(i-1, replace);
+//				
+//					
+//		System.out.println(grades);
+//				
+//			}
+//				
+//				
+//			}
+//			
+//		}
+//		
+//	
+//		return grades;
+//		
+//	}
+	
+	
+	
+//	public static List<String> sortDNA(List<String> dna){
+//		for(int I=0;I<dna.size();I++) {
+//			for(int i=1;i<dna.size();i++) {
+//				if(dna.get(i).length()<dna.get(i-1).length()) {
+//					String replace = dna.get(i);
+//					dna.set(i, dna.get(i-1));
+//					dna.set(i-1, replace);
+//				}
+//			}
+//	}
+//		
+//		
+//		return dna;
+//	}
+	
+	
+	
+	public static List<String> sortWords(List<String> words){
+		for(int I=0;I<words.size();I++) {
+			for(int i=1;i<words.size();i++) {
+				if(words.get(i).compareTo(words.get(i-1))<0)  {
+					String replace = words.get(i);
+					words.set(i, words.get(i-1));
+					words.set(i-1, replace);
+				}
+			}
+		}
+		return words;
+	}
 	
 	
 	
